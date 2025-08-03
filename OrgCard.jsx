73355fa615e3fc6c827bd26fb48ccb5297039079
HemantAAkotkar <163880289+HemantAAkotkar@users.noreply.github.com>
2025-08-03 22:17:57 +0530

    Create OrgCard.jsx

---
+++ OrgCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Projectcard from "./projectcard";
+export default function (){
+    return(
+        <div className="Card">
+        <Projectcard name="Agriculture"  />
+        <Projectcard/>
+        <Projectcard/>
+        
+        </div>
+    );
+}
